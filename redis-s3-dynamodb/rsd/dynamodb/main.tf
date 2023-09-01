2b5798f5bc8bf012b0843d9cdcd611159b855475
Damola12345 <ajiboladamola789@gmail.com>
2023-09-01 12:19:15 +0100

    dynamodb

---
+++ redis-s3-dynamodb/rsd/dynamodb/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.47"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-redis-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
