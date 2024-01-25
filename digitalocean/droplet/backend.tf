1473fd4981c2f6c7645f8d6d1ef7f5b79447c1ba
Damola12345 <ajiboladamola789@gmail.com>
2024-01-25 08:14:24 +0100

    digital ocean droplets

---
+++ digitalocean/droplet/backend.tf
@@ -0,0 +1,26 @@
+# Backend to store state file
+
+terraform {
+  required_version = ">= 0.13.0"
+  backend "s3" {
+    endpoint                    = "fra1.digitaloceanspaces.com"
+    region                      = "fra1"
+    bucket                      = "dam-terraform-state"
+    key                         = "dam-vpc"
+    access_key                  = ""
+    secret_key                  = ""
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+  }
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = ">= 2.19.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.2.2"
+    }
+  }
+}
---
+++ digitalocean/droplet/main.tf
@@ -0,0 +1,66 @@
+## DOK
+
+# Create a Vpc in the fra1 region
+module "vpc" {
+  source      = "terraform-do-modules/vpc/digitalocean"
+  name        = "dam"
+  environment = "dev"
+  region      = "fra1"
+  ip_range    = "10.10.0.0/16"
+}
+
+provider "digitalocean" {
+  token = ""
+}
+
+# Create a new Web Droplet in the fra1 region
+resource "digitalocean_droplet" "vm" {
+  image    = "ubuntu-22-04-x64"
+  name     = "dam-dev-vm"
+  region   = "fra1"
+  size     = "s-1vcpu-1gb"
+  vpc_uuid = module.vpc.id
+  ssh_keys = [""]
+
+}
+
+# Create firewall for Droplet in the fra1 region
+resource "digitalocean_firewall" "testcloud" {
+  name = "dam-dev-fw"
+
+  droplet_ids = [digitalocean_droplet.vm.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8080"
+    source_addresses = ["", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
